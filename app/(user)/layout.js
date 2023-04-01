9dd355e13bbaee383618d5177e6f91be9c4a73c0
Zayn Chowdhury <zaynchowdhury763@gmail.com>
2023-03-31 18:01:14 -0700

    a

--- app/(user)/layout.js
+++ app/(user)/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
       <body>
         <Header />
         {children}
-        {/* <Footer /> */}
+        <Footer />
       </body>
     </html>
   );
