c7230acf7967951725dda9c77de7c7340209015d
Zayn Chowdhury <zaynchowdhury763@gmail.com>
2023-04-08 19:18:19 -0700

    popup responsiveness

--- components/Popup.js/Popup.js
+++ components/Popup.js/Popup.js
@@ -7,23 +7,23 @@ import CloseIcon from "@mui/icons-material/Close";
 const Popup = ({ setPopupVisibility }) => {
   return (
     <div className="md:w-[900px] w-[90vw] md:h-[450px] h-[90vh] bg-yellow mx-auto relative flex items-center md:flex-row flex-col">
-      <div className="md:w-1/2 w-full relative h-full bg-yellow ">
+      <div className="md:w-1/2 w-full relative md:h-full h-[30vh] bg-yellow ">
         <Image src="/gift.png" fill className="hidden md:block" />
         <Image src="/gift-mob.png" fill className="md:hidden block" />
       </div>
-      <div className="md:w-1/2 relative h-[450px] flex flex-col items-start justify-center md:pr-8 p-6">
+      <div className="md:w-1/2 relative md:h-[450px] h-[60vh] flex flex-col items-start justify-center md:pr-8 p-6">
         <h2 className="inline-block text-left mb-4">
-          <span className="md:text-[14px] text-[12px]">Get Upto</span>
+          <span className="md:text-[14px] text-[1.5vh]">Get Upto</span>
           <br />
-          <span className="font-lora font-semibold md:text-[85px] !text-[65px] md:leading-[90px] leading-[65px]">
+          <span className="font-lora font-semibold md:text-[85px] text-[9vh] md:leading-[90px] leading-[65px]">
             50% OFF
           </span>
           <br />
-          <span className="md:text-[50px] text-[30px] md:leading-[0.95] leading-[32px]">
+          <span className="md:text-[50px] text-[4vh] md:leading-[0.95] leading-[32px]">
             In Commissions
           </span>
         </h2>
-        <P>
+        <P className="md:text-inherit text-[2vh]">
           Become a part of Bangladesh’s first customized client reward program.
           A step towards acknowledging the valuable contributions of our
           respected clients.
